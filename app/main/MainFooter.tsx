85e4a4d01032e0afd64eb52eade50604d787e9c4
Brian <me@brianbrennan.com>
2021-07-15 15:46:36 -0400

    making a weird stripe thing? Don't Know

--- app/main/MainFooter.tsx
+++ app/main/MainFooter.tsx
@@ -6,7 +6,10 @@ export default class MainFooter extends React.Component {
     render() {
         return (
             <div className="bb-mainFooter">
-
+                <div className="bb-mainFooter-level1"/>
+                <div className="bb-mainFooter-level2"/>
+                <div className="bb-mainFooter-level3"/>
+                <div className="bb-mainFooter-level4"/>
             </div>
         );
     }
