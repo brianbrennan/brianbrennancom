e388b1d02aebd86029431b0b06c3dfc9a0bb6f7a
Brian Brennan <me@brianbrennan.com>
2021-08-08 16:42:24 -0400

    Favicon

--- app/globals/project-cards/RepoCard.tsx
+++ app/globals/project-cards/RepoCard.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
 
+import './RepoCard.scss';
+
 export default class RepoCard extends React.Component {
-    
+    render() {
+        return (
+            <div className="bb-repoCard">
+
+            </div>
+        );
+    }
 }
---
+++ app/project/Project.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default class Project extends React.Component {
+
+}
