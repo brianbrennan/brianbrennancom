3ecca26ccd1f16066e802c8754129d592699272a
Brian Brennan <me@brianbrennan.com>
2021-08-13 17:34:00 -0400

    Fixing some potential deploy stuff

--- webpack.config.js
+++ webpack.config.js
@@ -49,34 +49,34 @@ let config = {
                     'resolve-url-loader',
                 ]
             },
-            // {
-            //     test: /\.png$/,
-            //     use: 'url-loader?limit=100000'
-            // },
-            // {
-            //     test: /\.jpg$/,
-            //     use: 'file-loader'
-            // },
-            // {
-            //     test: /\.gif$/,
-            //     use: 'file-loader'
-            // },
-            // {
-            //     test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
-            //     use: 'url-loader?limit=10000&mimetype=application/font-woff'
-            // },
-            // {
-            //     test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-            //     use: 'url-loader?limit=10000&mimetype=application/octet-stream'
-            // },
-            // {
-            //     test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-            //     use: 'file-loader'
-            // },
-            // {
-            //     test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-            //     use: 'url-loader?limit=10000&mimetype=image/svg+xml'
-            // }
+            {
+                test: /\.png$/,
+                use: 'url-loader?limit=100000'
+            },
+            {
+                test: /\.jpg$/,
+                use: 'file-loader'
+            },
+            {
+                test: /\.gif$/,
+                use: 'file-loader'
+            },
+            {
+                test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+                use: 'url-loader?limit=10000&mimetype=application/font-woff'
+            },
+            {
+                test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+                use: 'url-loader?limit=10000&mimetype=application/octet-stream'
+            },
+            {
+                test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+                use: 'file-loader'
+            },
+            {
+                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+                use: 'url-loader?limit=10000&mimetype=image/svg+xml'
+            }
         ]
     },
     resolve: {
